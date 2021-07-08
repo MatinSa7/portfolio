e97d3ed3d396715927a238de6dc4b572bb0dd9ac
Matin Sarahi <matinsarahi@gmail.com>
2021-07-08 15:26:19 -0400

    background line animations

--- src/page/IntroPage.js
+++ src/page/IntroPage.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useLocation } from "react-router-dom";
 
 const IntroPage = () => {
+  const { pathname } = useLocation();
+  const [hoverStatus, setHoverStatus] = useState(0);
   return (
     <StyledIntroPageContainer>
       <StyledLogoArea>
@@ -15,8 +18,27 @@ const IntroPage = () => {
           assumenda. Delectus.
         </p>
         <StyledList>
-          <a href="">balls</a>
-          <a href="">tentacles</a>
+          <StyledLineContainer
+            onMouseEnter={() => setHoverStatus(1)}
+            onMouseLeave={() => setHoverStatus(0)}
+          >
+            <a href="">balls</a>
+            {hoverStatus === 1 && <StyledLine></StyledLine>}
+          </StyledLineContainer>
+          <StyledLineContainer
+            onMouseEnter={() => setHoverStatus(2)}
+            onMouseLeave={() => setHoverStatus(0)}
+          >
+            <a href="">tentacles</a>
+            {hoverStatus === 2 && <StyledLine></StyledLine>}
+          </StyledLineContainer>
+          <StyledLineContainer
+            onMouseEnter={() => setHoverStatus(3)}
+            onMouseLeave={() => setHoverStatus(0)}
+          >
+            <a href="">tentacles</a>
+            {hoverStatus === 3 && <StyledLine></StyledLine>}
+          </StyledLineContainer>
         </StyledList>
       </StyledDescription>
     </StyledIntroPageContainer>
@@ -30,20 +52,27 @@ const StyledIntroPageContainer = styled(motion.div)`
 
 const StyledList = styled(motion.ul)`
   display: flex;
-  flex: 1 1 5rem;
+  flex: 1 1 6rem;
   flex-direction: column;
   padding-left: 5rem;
+  a {
+    font-size: calc(0.8vw + 1rem);
+    padding-bottom: 0.5rem;
+    text-decoration: none;
+    color: black;
+  }
 `;
 
 const StyledDescription = styled(motion.div)`
   display: flex;
   align-items: center;
   justify-content: center;
-  min-height: 40vh;
+  min-height: 50vh;
 
   p {
     flex: 5 1 20rem;
-    font-size: calc(1vw + 1rem);
+    font-size: calc(2vw + 1rem);
+    font-weight: 600;
   }
 `;
 
@@ -55,6 +84,20 @@ const StyledLogoArea = styled(motion.div)`
   align-items: center;
   min-height: 20vh;
   margin: auto;
+`;
+
+const StyledLineContainer = styled(motion.div)`
+  position: relative;
+  margin: 0.5rem 0;
+`;
+
+const StyledLine = styled(motion.div)`
+  position: absolute;
+  background: #8f6aff;
+  bottom: 0%;
+  z-index: -1;
+  height: 40%;
+  width: 100%;
 `;
 
 export default IntroPage;
