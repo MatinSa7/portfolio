8a22fd471927ccc22673cedbb409d9f632f723f7
Matin Sarahi <matinsarahi@gmail.com>
2021-08-16 15:43:38 -0400

    Projects Page additions

--- src/page/ProjectsPage.js
+++ src/page/ProjectsPage.js
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import styled, { keyframes } from "styled-components";
 import { motion } from "framer-motion";
 import { useLocation } from "react-router-dom";
+import invigilationscript from "../image/invig.png";
+import schedulephoto from "../image/schedule photo.png";
+import smpsolver from "../image/smpsolver.png";
+
 
 const ProjectsPage = () => {
   return (
@@ -9,17 +13,39 @@ const ProjectsPage = () => {
       <h1>Projects:</h1>
       <StyledCardContainer>
         <motion.a href="" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
-          test
+          Exam Invigilation Scheduling
+        </motion.a>
+        <StyledImageContainer>
+          <img src={invigilationscript} alt="" />
+          <img src={schedulephoto} alt="" />
+        </StyledImageContainer>
+      </StyledCardContainer>
+      <StyledCardContainer>
+        <motion.a whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} href="">
+          Stable Matching Solver
+        </motion.a>
+        <StyledImageContainer>
+          <img src={smpsolver} alt="" />
+        </StyledImageContainer>
+      </StyledCardContainer>
+      <StyledCardContainer>
+        <motion.a whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} href="">
+          CLNx Career Portal
         </motion.a>
       </StyledCardContainer>
       <StyledCardContainer>
         <motion.a whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} href="">
-          test
+          Pulsate
         </motion.a>
       </StyledCardContainer>
       <StyledCardContainer>
         <motion.a whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} href="">
-          test
+          Mattify
+        </motion.a>
+      </StyledCardContainer>
+      <StyledCardContainer>
+        <motion.a whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} href="">
+          Gym Booking Automation
         </motion.a>
       </StyledCardContainer>
     </StyledProjectsPageContainer>
@@ -48,6 +74,8 @@ const StyledCardContainer = styled(motion.div)`
   }
 `;
 
-const StyledImageContainer = styled(motion.div)``;
+const StyledImageContainer = styled(motion.div)`
+  margin-left: 10rem;
+`;
 
 export default ProjectsPage;
