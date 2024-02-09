c94fec897f82ede02aea4f81a805ba12bcdd3f3b
MatinSa7 <matinsarahi@gmail.com>
2024-02-09 18:33:18 -0500

    resume change

--- src/page/IntroPage.js
+++ src/page/IntroPage.js
@@ -42,7 +42,7 @@ const IntroPage = () => {
           whileHover={{ scale: 1.1 }}
           whileTap={{ scale: 0.9 }}
           target="_blank"
-          href="https://drive.google.com/file/d/1hJlswgG2ywe6ihdVWrOIUyF46QG2P1vi/view?usp=sharing"
+          href="https://drive.google.com/file/d/1KsPJ5WrzFr16us18W4czz7S-HD5ssS45/view?usp=sharing"
         >
           Resume
         </motion.a>
